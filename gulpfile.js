231f77214eefdfe5863849d9e48d7609eba1418d
Colyn Brown <clbrown@godaddy.com>
2014-04-04 17:54:44 -0700

    designed new cdsm upload process; added experimental jade support

--- gulpfile.js
+++ gulpfile.js
@@ -2,61 +2,33 @@ var gulp = require('gulp');
 var swig = require('gulp-swig');
 var less = require('gulp-less');
 var cdsm = require('gulp-cdsm');
-var Cache = require('node-simple-cache');
-var prompt = require('prompt');
 var args = require('yargs').argv;
-var changed = require('gulp-changed');
-
-var rootAssetPath = (process.platform === 'win32') ? '\\\\g1dwimages001\\images\\fos\\201401\\' : '/Volumes/images/fos/201401/';
+var _ = require('lodash');
+var paths = require('./paths.json');
+var jade = require('gulp-jade');
 
 function getUserHome() {
   return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
 }
-var paths = {
-  templates: ['./src/sales/**/*.html'],
-  layouts: ['./src/layouts/**/*.html'],
-  less: './src/sales/**/*.less',
-  images: './src/sales/**/img/**/*',
-  json: ['./_globals.json', './src/sales/**/*.json'],
-  build: './build/sales/',
-  assets: rootAssetPath + 'sales/'
-};
 
-function cdsmAuth() {
-  var cache = new Cache({
-    path: getUserHome() + '/.cdsm-cache/'
-  });
-  var creds = cache.get('cdsm-creds') || false;
-  var schema = {
-    properties: {}
-  };
-  if (!creds) {
-    schema.properties = {
-      username: {
-        required: true,
-        message: 'Username'
-      },
-      password: {
-        hidden: true,
-        required: true,
-        message: 'Password'
-      },
-      savecreds: {
-        validator: /y|n/,
-        message: 'Save credentials? (y/N)',
-        default: 'n'
-      }
-    };
-  }
-
-  var prompt = require('prompt');
-
-  prompt.start();
-
-  prompt.get(schema, function(err, result) {
-    console.log(result);
-  });
+if (!args.src || _.indexOf(Object.keys(paths), args.src) === -1) {
+  console.log('Please set a source: (ex. gulp templates --src website-builder) \nAvailable sources:\n  ' + Object.keys(paths).join('\n  '));
+  process.exit(1);
 }
+
+var baseFilePath = paths[args.src];
+var theme = args.theme || 'scotty';
+var rootAssetPath = (process.platform === 'win32') ? '\\\\g1dwimages001\\images\\fos\\sales\\themes\\' + theme + '\\' : '/Volumes/images/fos/sales/themes/' + theme + '/';
+
+var paths = {
+  templates: ['./src/sales/' + baseFilePath + '/*.html'],
+  layouts: ['./src/layouts/' + baseFilePath + '/*.html'],
+  less: './src/sales/' + baseFilePath + '/css/**/*.less',
+  images: './src/sales/' + baseFilePath + '/img/**/*',
+  json: ['./_globals.json', './src/sales/' + baseFilePath + '/*.json'],
+  build: './build/sales/' + baseFilePath,
+  assets: rootAssetPath + baseFilePath
+};
 
 var swigOpts = {
   load_json: true,
@@ -69,20 +41,28 @@ var swigOpts = {
   }
 };
 
-gulp.task('templates', function(cb) {
-  //cdsmAuth(cb);
+gulp.task('templates', function() {
   gulp.src(paths.templates)
-    //.pipe(changed(paths.build))
     .pipe(swig(swigOpts))
     .pipe(cdsm())
+    .pipe(gulp.dest(paths.build))
+});
+
+/* Experimental Jade Template Rendering */
+gulp.task('jade', function() {
+  gulp.src(['./src/sales/' + baseFilePath + '/*.jade'])
+    .pipe(jade({
+      pretty: true,
+      locals: require('./_globals.json')
+    }))
     .pipe(gulp.dest(paths.build))
 });
 
 gulp.task('less', function() {
   gulp.src(paths.less)
     .pipe(less())
-    .pipe(gulp.dest(paths.assets))
-    .pipe(gulp.dest(paths.build));
+    .pipe(gulp.dest(paths.assets + '/css/'))
+    .pipe(gulp.dest(paths.build + '/css/'));
 });
 
 gulp.task('lessmin', function() {
@@ -90,26 +70,22 @@ gulp.task('lessmin', function() {
     .pipe(less({
       compress: true
     }))
-    .pipe(gulp.dest(paths.assets))
-    .pipe(gulp.dest(paths.build));
+    .pipe(gulp.dest(paths.assets + '/css/'))
+    .pipe(gulp.dest(paths.build + '/css/'));
 });
 
 gulp.task('images', function() {
   gulp.src(paths.images)
-    .pipe(gulp.dest(paths.assets))
-    .pipe(gulp.dest(paths.build));
+    .pipe(gulp.dest(paths.assets + '/img/'))
+    .pipe(gulp.dest(paths.build + '/img/'));
 });
-
-// gulp.task('cdsm', function() {
-//   gulp.src(paths.templates)
-//     .pipe(cdsm());
-// });
 
 gulp.task('watch', function() {
   gulp.watch(paths.templates, ['templates']);
-  //gulp.watch(paths.json, ['templates']);
+  gulp.watch(paths.json, ['templates']);
   gulp.watch(paths.less, ['less', 'lessmin']);
   gulp.watch(paths.images, ['images']);
 });
 
-gulp.task('default', ['templates', 'lessmin', 'less', 'watch']);
+gulp.task('assets', ['less', 'lessmin', 'images']);
+gulp.task('default', ['watch']);
