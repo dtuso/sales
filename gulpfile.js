82b93ef08e40ca7d7d208cebf634a7afa9b6df7e
Colyn Brown <clbrown@godaddy.com>
2014-04-23 19:53:57 -0700

    added back in a simplified CDS deployment method

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,7 @@ var swigLangOpts = {
 
 gulp.task('html', function() {
   gulp.src(paths.templates)
+    .pipe(changed(paths.build))
     .pipe(swig(swigTplOpts))
     .pipe(cdsm())
     .pipe(gulp.dest(paths.build));
